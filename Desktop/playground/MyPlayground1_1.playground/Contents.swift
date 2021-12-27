505cbb69db0f4911683aafff6ccca3a72da04450
Mikhail Lukyanchikov <gipermike@mail.ru>
2021-12-27 19:19:58 +0300

    inicial commit

---
+++ Desktop/playground/MyPlayground1_1.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+let name1 = "Vasya"
+let name2 = "Igor"
+let name3 = "Petya"
+
+let old1 = 23
+let old2 = 37
+let old3 = 23
+
+let suname1 = "Ivanov"
+let suname2 = "Petrov"
+let suname3 = "Zalupkin"
+
+print("first student Name is - \(name2); Suname is - \(suname2); \(old2) UearsOld")
+print("first student Name is - \(name3); Suname is - \(suname3); \(old3) UearsOld")
